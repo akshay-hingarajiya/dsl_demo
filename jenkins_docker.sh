b70820cb0488c794ea1274f2e25542bc846328ed
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-06-21 09:48:56 +0530

    Update jenkins_docker.sh

--- jenkins_docker.sh
+++ jenkins_docker.sh
@@ -7,5 +7,5 @@ docker run \
 -p 8080:8080 \
 -p 50000:50000 \
 -v jenkins-data:/var/jenkins_home \
--v //var/run/docker.sock:/var/run/docker.sock \
+-v usr/local/bin/docker:/usr/bin/docker \
 jenkinsci/blueocea
