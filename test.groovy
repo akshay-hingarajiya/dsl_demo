f519b0e2b77f02371559e3510436be6808828d69
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-06-28 16:19:57 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,6 +4,7 @@
 //after that we write a function for
 package javaposse.jobdsl.plugin
 
+import groovy.transform.Field
 import org.jenkinsci.plugins.workflow.job.*
 import javaposse.jobdsl.dsl.jobs.MultibranchWorkflowJob
 import javaposse.jobdsl.dsl.DslFactory
