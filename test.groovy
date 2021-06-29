0be9d871518c56da9ea3ff553741b1766ebe9bd7
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-06-29 15:30:59 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -22,10 +22,10 @@ import javaposse.jobdsl.plugin.structs.DescribableListContext
 def num=$Project
 
     // create CI jobs
-    if(num==10) {
+if("${Project}"==10) {
       createCIJobforPython('python_job','akshay-hingarajiya/dsl_demo','master')   //function call for run python project
     }
-    else if(num==9){
+    else if("${Project}"==9){
         // Pass params accordingly
       createDeployJob('node_demo','jenkins-docs/simple-java-maven-app','master')  //function call for run node project 
     }
