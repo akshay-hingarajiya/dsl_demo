59831f0e94569216fa9ab36dd0b2bc3cf1a84505
akshay-hingarajiya <83569095+akshay-hingarajiya@users.noreply.github.com>
2021-06-30 09:47:06 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -2,23 +2,7 @@
 //in this script we define a job for two project,python and nodejs. 
 //first we define a if else condition for which job is run.
 //after that we write a function for
-/*package javaposse.jobdsl.plugin
 
-import javaposse.jobdsl.dsl.Job
-import groovy.transform.Field
-import org.jenkinsci.plugins.workflow.job.*
-import javaposse.jobdsl.dsl.jobs.MultibranchWorkflowJob
-import javaposse.jobdsl.dsl.DslFactory
-import javaposse.jobdsl.dsl.Folder
-import javaposse.jobdsl.dsl.jobs.WorkflowJob
-import javaposse.jobdsl.dsl.DslScriptLoader
-import javaposse.jobdsl.plugin.JenkinsJobManagement
-import javaposse.jobdsl.dsl.ContextHelper
-import javaposse.jobdsl.dsl.DslContext
-import javaposse.jobdsl.dsl.JobParent
-import javaposse.jobdsl.dsl.helpers.ConfigFilesContext
-import javaposse.jobdsl.plugin.structs.DescribableListContext
-*/
 def P="${Project}"
 print (P)
     // create CI jobs
@@ -29,9 +13,9 @@ else if(P=='java_project'){
         // Pass params accordingly
       createDeployJob('java_project','jenkins-docs/simple-java-maven-app','master')  //function call for run node project 
     }
- /*   else {
-      print "No CI Jobs will be generated for "
-    }*/
+    else {
+      print "No CI Jobs will be generated"
+    }
   
 // this function def is for pyhon project 
 
@@ -45,36 +29,8 @@ def createCIJobforPython(def ciJobName,def projectGitUrlToRepo,def defaultBranch
         }
     }
 }
-/*    pipelineJob("${ciJobName}") {
-        print"hello"
-        
-    scm {
-      git {
-        remote {
-            url("${projectGitUrlToRepo}")
-   //       credentials(gitCredentials)
-        }
-        wipeOutWorkspace(true)
-        createTag(false)
-          branch("${defaultBranch}")
-      }
-    }
-    triggers {
-      cron("H * * * 1-5")
-    }
-   
-    steps {
-       bat 'docker build -t python-demo .'
-       bat 'docker run --name dsl -d -p 8181:8181 python-demo'
-       sleep(time:30,unit:"SECONDS") 
-       bat 'docker stop dsl'
-       bat 'docker container rm dsl'
-    }
- 
-  }
-}
-*/
-// this function def is for node project
+
+// this function def is for java project
 }
 def createDeployJob(def deployJobName,def projectGitUrlToRepo,def defaultBranch) {
   job("${deployJobName}") {
